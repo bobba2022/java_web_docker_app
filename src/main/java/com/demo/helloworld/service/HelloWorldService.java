14d159de76249ff347941e6348ae2d299c689ce2
MRaju2022 <98283443+MRaju2022@users.noreply.github.com>
2023-06-07 20:49:14 +0530

    Update HelloWorldService.java

--- src/main/java/com/demo/helloworld/service/HelloWorldService.java
+++ src/main/java/com/demo/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Kuberenetes Deployment(Blue & Green Deployement)";
+			return "Hello Kuberenetes Deployment(Blue & Green Deployement..)";
 		}else{
 			return "Hello " + name;
 		}
